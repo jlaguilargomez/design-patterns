42a94790ce48d509d6306788bc242264698cbb09
jlaguilargomez <jlaguigom@gmail.com>
2023-08-04 18:28:54 +0200

    create  base ts project

---
+++ dist/app.js
@@ -0,0 +1,7 @@
+"use strict";
+// src/app.ts
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+const message = greet('TypeScript');
+console.log(message);
---
+++ src/app.ts
@@ -0,0 +1,7 @@
+// src/app.ts
+function greet(name: string) {
+  return `Hello, ${name}!`;
+}
+
+const message = greet('TypeScript');
+console.log(message);
